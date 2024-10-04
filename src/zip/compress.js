1465b563218109e36b3b38178c08fa49fb9b178d
Aliaksei Krutsko <wonder4ik@gmail.com>
2024-10-04 21:52:49 +0300

    feat: implement zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,8 @@
-const compress = async () => {
-    // Write your code here 
-};
+import { createGzip, Gzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
 
-await compress();
+const readable = createReadStream(import.meta.dirname + '/files/fileToCompress.txt');
+const writable = createWriteStream(import.meta.dirname + '/files/archive.gz');
+
+pipeline(readable, createGzip(), writable).catch(console.error);
