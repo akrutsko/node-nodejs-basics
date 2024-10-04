6c45793679f7ce2236bdae9936fe4aaadb6d7b5a
Aliaksei Krutsko <wonder4ik@gmail.com>
2024-10-04 16:11:04 +0300

    feat: implement streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,12 @@
-const transform = async () => {
-    // Write your code here 
-};
+import { Transform } from 'node:stream';
 
-await transform();
+const transformable = new Transform({
+  transform(chunk, _, callback) {
+    const input = chunk.toString().trim();
+    const reverse = input.split('').reverse().join('');
+    this.push(reverse + '\n');
+    callback();
+  },
+});
+
+process.stdin.pipe(transformable).on('error', console.error).pipe(process.stdout).on('error', console.error);
