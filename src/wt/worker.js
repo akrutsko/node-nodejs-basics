383ae62229b035fad47f13543ba587413cd58b4b
Aliaksei Krutsko <wonder4ik@gmail.com>
2024-10-06 18:52:00 +0300

    fix: remove throwing error

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -2,7 +2,4 @@ import { parentPort, workerData } from 'node:worker_threads';
 
 const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
 
-if (workerData === 10) {
-  throw new Error();
-}
 parentPort.postMessage(nthFibonacci(workerData));
